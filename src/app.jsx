db451e4569c450af039b0155417c2e98783b0dd9
aayambhatt <aayambhatt23@gmail.com>
2025-02-10 14:25:34 +0530

    routing, outlet

--- src/app.jsx
+++ src/app.jsx
@@ -2,21 +2,48 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./Components/Header";
 import Body from "./Components/Body";
-
-
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import Error from "./Components/Error";
+import RestaurantMenu from "./Components/RestaurantMenu";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 const Applayout = () => {
-    return (
-        <div className="app">
-            <Header/>
-            <Body/>
-        </div>
-    )
-}
+  return (
+    <div className="app">
+      <Header/>
 
+    <Outlet/>
+    </div>
+  );
+};
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Applayout/>,
+    children: [
+      {
+        path: "/",
+        element: <Body/>
+      },
+      {
+        path: "/about",
+        element: <About/>,
+      },
+      {
+        path: "/contact",
+        element: <Contact/>
+      },
+      {
+        path: "/restaurants/:resId",
+        element: <RestaurantMenu/>
+
+      }
+    ],
+    errorElement: <Error/>
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-
-root.render(<Applayout/>);
-
+root.render(<RouterProvider router={appRouter}/>);
