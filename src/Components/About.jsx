e8da1abb823dcbe88d2e05c3e2fa3420f4ccadc4
aayambhatt <aayambhatt23@gmail.com>
2025-02-15 15:28:47 +0530

    User class

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,14 +1,46 @@
 import User from "./User";
+import UserClass from "./UserClass";
+import { Component } from "react";
 
-const About = () => {
 
-    return (
-        <div>
-            <h1>About</h1>
-            <h2>This is my project</h2>
-            <User/>
-        </div>
-    )
+class About extends Component {
+
+    constructor(props) {
+        super(props);
+
+        console.log("Parent constructor");
+
+    };
+
+    componentDidMount(){
+        console.log("Parent component did mount");
+    }
+
+    render(){
+        console.log("Parent render");
+        return (
+            <div>
+                <h1>About</h1>
+                <h2>This is my project</h2>
+               <UserClass name={"Aayam(Class)"}/>
+            </div>
+        )
+        
+    }
 }
+
+
+
+
+// const About = () => {
+
+    // return (
+    //     <div>
+    //         <h1>About</h1>
+    //         <h2>This is my project</h2>
+    //        <UserClass name={"Aayam(Class)"}/>
+    //     </div>
+    // )
+// }
 
 export default About;
