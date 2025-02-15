f35bb32b2089b6245a2ef1e41974b4ede7bdd0ed
aayambhatt <aayambhatt23@gmail.com>
2025-02-15 15:29:29 +0530

    Hooks and Links

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,13 @@
 import { LOGO_URL } from "../utils/constants";
 import { useState } from "react";
+import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Header = () => {
 
     const [btnNameReact, setBtnNameReact] = useState("Login");
+    
+    const onlineStatus = useOnlineStatus();
 
     return (
         <div className="header">
@@ -12,9 +16,11 @@ const Header = () => {
             </div>
             <div className="nav-items">
                 <ul>
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li>Contact Us</li>
+                    <li>Online Status: {onlineStatus ? "🟢" : "🔴" }</li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About Us</Link></li>
+                    <li><Link to="/contact">Contact Us</Link></li>
+                    <li><Link to="/grocery"> Grocery Mart</Link></li>
                     <li>Cart</li>
                     <button className="login" onClick={() => {
                     btnNameReact === "Login" ? setBtnNameReact("Logout") : setBtnNameReact("Login")
@@ -26,4 +32,4 @@ const Header = () => {
 
 };
 
-export default Header;
+export default Header; 
